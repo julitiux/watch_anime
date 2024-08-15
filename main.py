ad257f747988a4a154e99a5069bc2b6e6a26cdc1
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-08-15 00:46:55 -0600

    show each element

--- main.py
+++ main.py
@@ -2,3 +2,4 @@ from animeflv import AnimeFLV
 with AnimeFLV() as api:
     elements = api.search(input("serie: "))
     for i, elements in enumerate(elements):
+        print(f"{i}, {elements.title}")
