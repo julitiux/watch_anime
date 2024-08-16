edc651870f7ebf597543723ac2f497a1a97b601d
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-08-15 19:26:01 -0600

    try to do something

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ with AnimeFLV() as api:
     elements = api.search(input("serie: "))
     for i, elements in enumerate(elements):
         print(f"{i}, {elements.title}")
+        try:
+            pass
+        except Exception as e:
+            print(e)
